ec7e45fe94f062fb98f808690cccaca2f08e1799
vshtuka <59761480+vshtuka@users.noreply.github.com>
2022-03-31 09:38:29 +0300

    Initial commit

---
+++ app/src/main/java/com/vladshtuka/explainer/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.vladshtuka.explainer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
