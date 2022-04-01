753c609632d5a07d80d7f8f6f7a83ab4fe08855f
vshtuka <59761480+vshtuka@users.noreply.github.com>
2022-04-01 16:42:31 +0300

    [EXP-2] Added home screen view

--- app/src/main/java/com/vladshtuka/explainer/presentation/screen_home/HomeFragment.kt
+++ app/src/main/java/com/vladshtuka/explainer/presentation/screen_home/HomeFragment.kt
@@ -5,16 +5,49 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.vladshtuka.explainer.R
+import com.vladshtuka.explainer.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_home, container, false
+        )
+        setContinueButton()
+        setNewGameButton()
+        setRulesButton()
+
+        return binding.root
+    }
+
+    private fun setContinueButton() {
+        binding.homePageContinueButton.setOnClickListener {
+            this.findNavController()
+                .navigate(HomeFragmentDirections.actionHomeFragmentToStartGameFragment())
+        }
+    }
+
+    private fun setNewGameButton() {
+        binding.homePageNewGameButton.setOnClickListener {
+            this.findNavController()
+                .navigate(HomeFragmentDirections.actionHomeFragmentToNewGameFragment())
+        }
+    }
+
+    private fun setRulesButton() {
+        binding.homePageRulesButton.setOnClickListener {
+            this.findNavController()
+                .navigate(HomeFragmentDirections.actionHomeFragmentToRulesFragment())
+        }
     }
 
 }
